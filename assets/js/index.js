a16fa060d1b522fe09ae9c7ce7eef929219fef45
tylerphopho <tyler.phopho@gmail.com>
2020-01-30 02:10:12 -0700

    First commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".slider").slider({
+        indicators: false,
+        height: 500,
+        transition: 500,
+        interval: 6000,
+    });
+    $(".modal").modal();
+})
